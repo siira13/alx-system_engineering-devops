7f85dc3ecf1ac809686abdb03f3dfc46b1aecf96
Fikirte M. Manaye <fikirross2010@gmail.com>
2021-08-20 00:05:01 +0300

    basic

---
+++ 0x00-shell_basics/burguer.java
@@ -0,0 +1,13 @@
+//imort java.util.Scanner;
+public class burguer
+{
+   public static void main(String[] args)
+   {
+   	System.out.println("Total is :" + (args[0] + args[1]));
+   	int args1=Integer.parseInt(args[0]);
+   	int args2=Integer.parseInt(args[1]);
+
+   	System.out.println("Total is :" + (args1 + args2));
+
+   }
+}
